0a096a4641c3879f1022c08c4a35e985f7694118
Amit Kumar <amitgodara1008@gmail.com>
2024-04-13 21:46:23 +0530

    shopify authorization

--- hackathon_instant/hackathon_instant.py
+++ hackathon_instant/hackathon_instant.py
@@ -1,14 +1,12 @@
-"""Welcome to Reflex!."""
-
-# Import all the pages.
 from datetime import datetime
-
 from sqlmodel import Field
 from hackathon_instant.pages import *
 
 import reflex as rx
 
 from sqlalchemy import Column, DateTime
+
+from .shopify_service import install_shopify_app, shopify_callback
 
 class State(rx.State):
     """Define empty state to allow access to rx.State.router."""
@@ -21,6 +19,8 @@ class StoreData(rx.Model, table=True):
      is_app_install: bool = Field(nullable=False, default=False)
      created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False))
      updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False, onupdate=datetime.now))
-    
+
 # Create the app.
 app = rx.App()
+app.api.add_api_route("/install-app/{shop_name}", install_shopify_app)
+app.api.add_api_route("/auth/shopify/callback", shopify_callback)
---
+++ hackathon_instant/shopify_service.py
@@ -0,0 +1,67 @@
+import binascii
+from http.client import HTTPException
+import os
+from fastapi.responses import RedirectResponse
+import requests
+import shopify
+import urllib.parse
+
+session_repository = dict()
+
+shop_name = "hackathon-instant"
+shopify_api_key = "7a84338d7b85067ec1f557c70313ff47"
+shopify_api_secret = "1a9e0c7a634380de0b56bb6ec7119cb0"
+
+def install_shopify_app(shop_name: str):
+    if not shop_name:
+        raise HTTPException(status_code=400, detail="Shop parameter is missing")
+    
+    print(shopify_api_key, shopify_api_secret)
+    
+    shopify.Session.setup(api_key=shopify_api_key, secret=shopify_api_secret)    
+    shop_url = "quickstart-4cb07f3a.myshopify.com"
+    api_version = '2024-01'
+    state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
+    redirect_uri = "https://2546-112-196-47-10.ngrok-free.app/auth/shopify/callback"
+    scopes = ['read_products', 'read_orders']
+
+    newSession = shopify.Session(shop_url, api_version)
+    auth_url = newSession.create_permission_url(scopes, redirect_uri, state)
+    print(auth_url)
+        
+    return RedirectResponse(auth_url)
+
+
+def shopify_callback(code: str | None = None, shop: str | None = None, state: str | None = None, hmac: str | None = None, host: str | None = None, timestamp: int | None = None):
+
+    if not code or not shop or not state:
+        raise HTTPException(status_code=400, detail="Missing code, shop, or state parameter")
+    
+    accessTokenUrl = f"https://{shop}/admin/oauth/access_token"
+    accessParams = {
+        "client_id": shopify_api_key,
+        "client_secret": shopify_api_secret,
+        "code": code,
+    }
+
+    response = requests.post(accessTokenUrl, params=urllib.parse.urlencode(accessParams))
+    data = response.json()
+
+    access_token = data["access_token"]
+    store_name = shop.split(".")[0]
+    print("Access Token:", access_token)
+    return {"message": "Shopify Auth Callback"}
+
+    # print(request_params)
+    # session = shopify.Session(shop, api_version)
+    # print("session:", session)
+    # access_token = session.request_token(request_params)
+    # print("Access Token:", access_token)
+    return {"message": "Shopify Auth Callback"}
+
+
+async def find_one_store(store_name: str):
+    # Logic to find store data from the database
+    pass
+
+    
