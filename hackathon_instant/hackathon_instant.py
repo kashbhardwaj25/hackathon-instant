b91f1b8606f5c177c7fbbcf5ce05cdace568353c
Amit Kumar <amitgodara1008@gmail.com>
2024-04-14 03:02:09 +0530

    add/update data in db

--- hackathon_instant/hackathon_instant.py
+++ hackathon_instant/hackathon_instant.py
@@ -12,14 +12,6 @@ class State(rx.State):
     """Define empty state to allow access to rx.State.router."""
     
 
-class StoreData(rx.Model, table=True):
-     id: str = Field(primary_key = True, nullable = False, unique = True,)
-     store_name: str = Field(nullable=False, unique = True)
-     access_token: str = Field(nullable=True)    
-     is_app_install: bool = Field(nullable=False, default=False)
-     created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False))
-     updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False, onupdate=datetime.now))
-
 # Create the app.
 app = rx.App()
 app.api.add_api_route("/install-app/{shop_name}", install_shopify_app)
--- hackathon_instant/shopify_service.py
+++ hackathon_instant/shopify_service.py
@@ -1,38 +1,54 @@
 import binascii
 from http.client import HTTPException
 import os
+import uuid
 from fastapi.responses import RedirectResponse
 import requests
 import shopify
 import urllib.parse
 
-session_repository = dict()
+from sqlalchemy import Column, DateTime
+from datetime import datetime
+from sqlmodel import Field
+import reflex as rx
 
-shop_name = "hackathon-instant"
+class StoreData(rx.Model, table=True):
+     id: str = Field(primary_key = True, nullable = False, unique = True, default=uuid.uuid4())
+     store_name: str = Field(nullable=False, unique = True)
+     access_token: str = Field(nullable=True)    
+     is_app_install: bool = Field(nullable=False, default=False)
+     created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False))
+     updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False, onupdate=datetime.now))
+
 shopify_api_key = "7a84338d7b85067ec1f557c70313ff47"
 shopify_api_secret = "1a9e0c7a634380de0b56bb6ec7119cb0"
 
-def install_shopify_app(shop_name: str):
+async def install_shopify_app(shop_name: str):
     if not shop_name:
         raise HTTPException(status_code=400, detail="Shop parameter is missing")
     
-    print(shopify_api_key, shopify_api_secret)
+    stores = await find_one_store(shop_name)
+    
+    if not stores:
+        await create_store(shop_name, is_app_install=False, access_token=None)
+        
+    if stores and stores.is_app_install:
+        raise HTTPException(status_code=400, detail="App already installed for this store")
     
     shopify.Session.setup(api_key=shopify_api_key, secret=shopify_api_secret)    
     shop_url = "quickstart-4cb07f3a.myshopify.com"
     api_version = '2024-01'
     state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
-    redirect_uri = "https://2546-112-196-47-10.ngrok-free.app/auth/shopify/callback"
+    redirect_uri = "https://6d64-112-196-47-10.ngrok-free.app/auth/shopify/callback"
     scopes = ['read_products', 'read_orders']
 
     newSession = shopify.Session(shop_url, api_version)
     auth_url = newSession.create_permission_url(scopes, redirect_uri, state)
-    print(auth_url)
         
     return RedirectResponse(auth_url)
 
 
-def shopify_callback(code: str | None = None, shop: str | None = None, state: str | None = None, hmac: str | None = None, host: str | None = None, timestamp: int | None = None):
+async def shopify_callback(code: str | None = None, shop: str | None = None, state: str | None = None, hmac: str | None = None, host: str | None = None, timestamp: int | None = None):
     if not code or not shop or not state:
         raise HTTPException(status_code=400, detail="Missing code, shop, or state parameter")
     
@@ -48,11 +64,32 @@ def shopify_callback(code: str | None = None, shop: str | None = None, state: st
 
     access_token = data["access_token"]
     store_name = shop.split(".")[0]
-    print("Access Token:", access_token)
+    
+    stores = await find_one_store(store_name)
+    
+    if not stores:
+        raise HTTPException(status_code=400, detail="Store not found")
+    
+    await update_store(store_name, is_app_install=True, access_token=access_token)
+    
     return {"message": "Shopify Auth Callback"}
 
 async def find_one_store(store_name: str):
-    # Logic to find store data from the database
-    pass
+    with rx.session() as session:
+        result = session.exec(StoreData.select().where(StoreData.store_name == store_name))
+        store =  result.first()
+        return store
 
-    
+async def create_store(store_name: str, access_token: str, is_app_install: bool):
+    with rx.session() as session:
+        store = StoreData(store_name=store_name, access_token=access_token, is_app_install=is_app_install)
+        session.add(store)
+        session.commit()
+        
+async def update_store(store_name: str, is_app_install: bool, access_token: str):
+    with rx.session() as session:
+        result = session.exec(StoreData.select().where(StoreData.store_name == store_name))
+        store = result.first()
+        store.is_app_install = is_app_install
+        store.access_token = access_token
+        session.commit()
