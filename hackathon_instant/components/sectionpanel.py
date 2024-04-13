ff252b948fa4a8ef17097a043925b2ca2fa04a15
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-04-14 00:52:46 +0530

    Section Panel Changes

--- hackathon_instant/components/sectionpanel.py
+++ hackathon_instant/components/sectionpanel.py
@@ -10,20 +10,19 @@ def sectionpanel() -> rx.Component:
     """
     # Get all the decorated pages and add them to the sidebar.
     return rx.vstack(
-      rx.heading("Sections", size="6"),
+      rx.heading("Sections", size="5", class_name="py-2"),
       rx.flex(
-              rx.card("Header", class_name="cursor-pointer"),
-              rx.card("Carousel", class_name="cursor-pointer"),
-              rx.card("Featured Product", class_name="cursor-pointer"),
-              rx.card("Contact Us", class_name="cursor-pointer"),
-              rx.card("Footer", class_name="cursor-pointer"),
-
+              rx.card("Header", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
+              rx.card("Carousel", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
+              rx.card("Featured Product", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
+              rx.card("Contact Us", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
+              rx.card("Footer", class_name="cursor-pointer transition-all hover:bg-rose-50"),
               spacing="2",
               width="100%",
               direction="column"
     ),
     rx.box(
-      rx.heading("Sections", size="6"),
+      rx.heading("Attributes", size="5", class_name="py-2"),
     ),
     class_name="border-l h-screen p-4 w-[300px]"
 )
