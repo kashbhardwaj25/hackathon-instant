492327da4aa7aa406571a717a4e846083a43843b
Aakash Bhardwaj <aakashbhardwaj643@gmail.com>
2024-04-14 03:11:22 +0530

    Section Panel State Setup

--- hackathon_instant/components/sectionpanel.py
+++ hackathon_instant/components/sectionpanel.py
@@ -2,6 +2,12 @@
 
 import reflex as rx
 
+class SectionPanelState(rx.State):
+    section: str = ''
+
+    def set_section(self, selectedSection):
+        self.section = selectedSection
+
 def sectionpanel() -> rx.Component:
     """The sectionpanel.
 
@@ -12,11 +18,11 @@ def sectionpanel() -> rx.Component:
     return rx.vstack(
       rx.heading("Sections", size="5", class_name="py-2"),
       rx.flex(
-              rx.card("Header", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
-              rx.card("Carousel", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
-              rx.card("Featured Product", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
-              rx.card("Contact Us", class_name="cursor-pointer hover:transition-all hover:bg-rose-50"),
-              rx.card("Footer", class_name="cursor-pointer transition-all hover:bg-rose-50"),
+              rx.card("Header", class_name="cursor-pointer hover:transition-all hover:bg-rose-50", on_click=lambda: SectionPanelState.set_section('Header'),),
+              rx.card("Carousel", class_name="cursor-pointer hover:transition-all hover:bg-rose-50", on_click=lambda: SectionPanelState.set_section('Carousel')),
+              rx.card("Featured Product", class_name="cursor-pointer hover:transition-all hover:bg-rose-50", on_click=lambda: SectionPanelState.set_section('Featured Product')),
+              rx.card("Contact Us", class_name="cursor-pointer hover:transition-all hover:bg-rose-50", on_click=lambda: SectionPanelState.set_section('Contact Us')),
+              rx.card("Footer", class_name="cursor-pointer transition-all hover:bg-rose-50", on_click=lambda: SectionPanelState.set_section('Footer')),
               spacing="2",
               width="100%",
               direction="column"
@@ -24,5 +30,6 @@ def sectionpanel() -> rx.Component:
     rx.box(
       rx.heading("Attributes", size="5", class_name="py-2"),
     ),
+    rx.text(SectionPanelState.section),
     class_name="border-l h-screen p-4 w-[300px]"
 )
