4ccc2b12b49d529af2143137d4affc2b5b44eef1
Yash Sharma <y.ferrari450@gmail.com>
2024-04-14 23:44:22 +0530

    cleaned code

--- hackathon_instant/components/landingPage/header.py
+++ hackathon_instant/components/landingPage/header.py
@@ -4,22 +4,7 @@ from hackathon_instant.cookie import CookieState
 from hackathon_instant.models.user import store_list
 
 
-class FetchStores(rx.State):
-     
-    stores = []
-
-    async def fetch_store(self):
-        mycookie = await self.get_state(CookieState)
-        self.stores = await store_list(mycookie.custom_cookie[0])
-
-    def get_stores(self):
-        return self.stores
-
 def header() -> rx.Component:
-      
-      fetched_stores = FetchStores.get_stores
-      print(fetched_stores)
-            
 
       return rx.flex(
         rx.flex(
@@ -29,7 +14,6 @@ def header() -> rx.Component:
              class_name="items-center flex"
         ),
         rx.button("Login", color_scheme="violet",width="100px",size="3",on_click=rx.redirect("/login")),
-        rx.button("Get Stores", color_scheme="violet",width="100px",size="3",on_click={FetchStores.fetch_store}),
         justify="between",
         class_name="px-2 md:px-[80px] py-4 bg-[#EAE3F8] flex items-center"
         )
