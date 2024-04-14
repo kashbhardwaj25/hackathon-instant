7a3f93f4e36a403dd18dffa9f34179113f466c0d
Amit Kumar <amitgodara1008@gmail.com>
2024-04-14 15:05:36 +0530

    setup env

--- hackathon_instant/models/store_data.py
+++ hackathon_instant/models/store_data.py
@@ -13,11 +13,21 @@ from datetime import datetime
 from sqlmodel import Field
 import reflex as rx
 
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Constants
+SHOPIFY_API_KEY = os.getenv('SHOPIFY_API_KEY')
+SHOPIFY_API_SECRET = os.getenv('SHOPIFY_API_SECRET')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+FE_URL = os.getenv('FE_URL')
+
 class PageData(BaseModel):
     shop_name: str
     title: str
     body_html: str
-    page_id: int = None  # Optional: Include if updating an existing page
+    page_id: int = None
 
 class StoreData(rx.Model, table=True):
      id: str = Field(primary_key = True, nullable = False, unique = True, default=uuid.uuid4())
@@ -26,9 +36,6 @@ class StoreData(rx.Model, table=True):
      is_app_install: bool = Field(nullable=False, default=False)
      created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False))
      updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=datetime.now, nullable=False, onupdate=datetime.now))
-
-shopify_api_key = "7a84338d7b85067ec1f557c70313ff47"
-shopify_api_secret = "1a9e0c7a634380de0b56bb6ec7119cb0"
 
 async def install_shopify_app(shop_name: str):
     if not shop_name:
@@ -42,11 +49,11 @@ async def install_shopify_app(shop_name: str):
     if stores and stores.is_app_install:
         raise HTTPException(status_code=400, detail="App already installed for this store")
     
-    shopify.Session.setup(api_key=shopify_api_key, secret=shopify_api_secret)    
-    shop_url = "quickstart-4cb07f3a.myshopify.com"
-    api_version = '2023-10'
+    shopify.Session.setup(api_key=SHOPIFY_API_KEY, secret=SHOPIFY_API_SECRET)    
+    shop_url = f"{shop_name}.myshopify.com"
+    api_version = '2024-01'
     state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
-    redirect_uri = "https://d11b-112-196-47-10.ngrok-free.app/auth/shopify/callback"
+    redirect_uri = REDIRECT_URI
     scopes = ['read_products', 'write_products', 'read_orders', 'write_content', 'read_content']
 
     newSession = shopify.Session(shop_url, api_version)
@@ -61,8 +68,8 @@ async def shopify_callback(code: str | None = None, shop: str | None = None, sta
     
     accessTokenUrl = f"https://{shop}/admin/oauth/access_token"
     accessParams = {
-        "client_id": shopify_api_key,
-        "client_secret": shopify_api_secret,
+        "client_id": SHOPIFY_API_KEY,
+        "client_secret": SHOPIFY_API_SECRET,
         "code": code,
     }
 
@@ -79,7 +86,7 @@ async def shopify_callback(code: str | None = None, shop: str | None = None, sta
     
     await update_store(store_name, is_app_install=True, access_token=access_token)
     
-    return {"message": "Shopify Auth Callback"}
+    return RedirectResponse(FE_URL)
 
 async def fetch_all_products(store_name):
     store_data = await find_one_store(store_name)
@@ -102,14 +109,14 @@ async def fetch_all_products(store_name):
 
 
 async def publish_page(store_name: str):
-    print("store_name: ", store_name)
     # Retrieve store data from the database
     store_data = await find_one_store(store_name)
+    
     if not store_data or not store_data.is_app_install:
         raise HTTPException(status_code=404, detail="Store not found or app not installed")
 
     # Setup Shopify session
-    shopify.Session.setup(api_key=shopify_api_key, secret=shopify_api_secret)
+    shopify.Session.setup(api_key=SHOPIFY_API_KEY, secret=SHOPIFY_API_SECRET)
     session = shopify.Session(f"{store_name}.myshopify.com", "2024-01", store_data.access_token)
     shopify.ShopifyResource.activate_session(session)
 
@@ -132,6 +139,7 @@ async def publish_page(store_name: str):
 
     if success:
         page_url = f"https://{store_name}.myshopify.com/pages/{page.handle}"
+        
         return RedirectResponse(page_url)
     else:
         raise HTTPException(status_code=500, detail="Failed to publish page")
